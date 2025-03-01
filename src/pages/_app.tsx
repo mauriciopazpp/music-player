a5e0985e733b7cfd54ae9bb01ee3871851f4cfd3
Mauricio Paz <mauriciopazdev@gmail.com>
2025-03-01 13:17:20 -0300

    Global layout fixed

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,24 @@
 import type { AppProps } from "next/app";
 import Navbar from "@/components/ui/Navbar";
 import "@/styles/globals.css";
+import BadgeGallery from "@/components/ui/BadgeGallery";
+import SidebarDesktop from "@/components/ui/SidebarDesktop";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
     return (
         <>
             <Navbar />
-            <Component {...pageProps} />
+            <div className="min-h-screen grid grid-cols-1 md:grid-cols-[16rem_auto]">
+                <SidebarDesktop />
+                <main className="flex-1 p-10">
+                    <div className="flex justify-center pb-5">
+                        <BadgeGallery />
+                    </div>
+                    <div className="content">
+                        <Component {...pageProps} />
+                    </div>
+                </main>
+            </div>
         </>
     );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,11 @@
-import BadgeGallery from "@/components/ui/BadgeGallery";
-import SidebarDesktop from "@/components/ui/SidebarDesktop";
 import TopArtists from "@/components/ui/TopArtists";
 import Releases from "@/components/ui/Releases";
 
 export default function Home() {
     return (
-        <div className="min-h-screen grid grid-cols-1 md:grid-cols-[16rem_auto]">
-            <SidebarDesktop />
-            <main className="flex-1 p-10">
-                <div className="flex justify-center pb-5">
-                    <BadgeGallery />
-                </div>
-                <TopArtists />
-                <Releases />
-            </main>
-        </div>
+        <>
+            <TopArtists />
+            <Releases />
+        </>
     );
 }
