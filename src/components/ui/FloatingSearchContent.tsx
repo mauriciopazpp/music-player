9d1c19b26c74da97ee700fa30110317da65d4215
Mauricio Paz <mauriciopazdev@gmail.com>
2025-03-01 22:02:13 -0300

    floating search closing when click outside

--- src/components/ui/FloatingSearchContent.tsx
+++ src/components/ui/FloatingSearchContent.tsx
@@ -1,12 +1,35 @@
-"use client";
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
 import HorizontalArtistCard from "./floatingSearchContent/HorizontalArtistCard";
 import { FloatingSearchContentProps } from "@/types/FloatingSearrchContentProps";
 import Link from "next/link";
 
 export function FloatingSearchContent({ artists, onSelectArtist }: FloatingSearchContentProps) {
+    const [isVisible, setIsVisible] = useState(true);
+    const searchRef = useRef<HTMLDivElement>(null);
+
+    const handleClickOutside = (event: MouseEvent) => {
+        if (searchRef.current && !searchRef.current.contains(event.target as Node)) {
+            setIsVisible(false);
+        }
+    };
+
+    useEffect(() => {
+        document.addEventListener("mousedown", handleClickOutside);
+
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, []);
+
+    if (!isVisible) {
+        return null;
+    }
+
     return (
-        <div className="absolute left-0 top-16 w-full bg-neutral-900 border-b-neutral-700 text-white z-20 shadow-2xl shadow-neutral-950">
+        <div
+            ref={searchRef}
+            className="absolute left-0 top-16 w-full bg-neutral-900 border-b-neutral-700 text-white z-20 shadow-2xl shadow-neutral-950"
+        >
             <div className="m-6 md:px-60 lg:px-96">
                 {artists.map((artist) => (
                     <div key={artist.idArtist} className="artist-section hover:bg-neutral-700 rounded-2xl p-2">
